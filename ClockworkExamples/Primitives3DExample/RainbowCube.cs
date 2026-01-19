d33e5b08aa41b0200fd8606241d7bd1e36588997
Harper Rhett <harperrhett@gmail.com>
2026-01-19 12:19:40 -0600

    3D is rendering

---
+++ ClockworkExamples/Primitives3DExample/RainbowCube.cs
@@ -0,0 +1,19 @@
+﻿using Clockwork;
+using Clockwork.Graphics;
+using Clockwork.Graphics.Draw3D;
+using System.Numerics;
+
+public class RainbowCube : Entity
+{
+	public Vector3 Position;
+
+	public RainbowCube(Vector3 position)
+	{
+		Position = position;
+	}
+
+	public override void OnDraw()
+	{
+		Primitives3D.DrawCube(Position, 0.5f, Colors.HotPink);
+	}
+}
