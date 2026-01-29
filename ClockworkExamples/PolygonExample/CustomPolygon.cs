adf1171c0ad1d8aabb5e37e80d0c5fda73e8d4dc
Harper Rhett <harperrhett@gmail.com>
2026-01-29 14:20:37 -0600

    shader WIP

---
+++ ClockworkExamples/PolygonExample/CustomPolygon.cs
@@ -0,0 +1,60 @@
+﻿using Clockwork.Graphics;
+using Clockwork.Particles;
+using Clockwork.Shapes;
+
+public class CustomPolygon : PolygonShape
+{
+	// General
+	private int index;
+	private static int count;
+
+	// Settings
+	private const float radius = 16;
+
+	// Particles
+	private ParticleEngine2D particleEngine;
+
+	public CustomPolygon(int sideCount, Color color) : base(radius, sideCount, color)
+	{
+		// Self
+		index = count;
+		count++;
+
+		// Particles
+		particleEngine = new();
+		particleEngine.DrawLayer = -1;
+		particleEngine.IsStreaming = true;
+		particleEngine.StreamCooldownTime = 0.01f;
+		particleEngine.StreamFired += StreamParticle;
+		particleEngine.RenderAsPixel();
+		particleEngine.AddInitializer(ParticleInitializers.SetColors(Color, Color.DropAlpha()));
+		particleEngine.AddInitializer(ParticleInitializers.RandomizeDirection());
+		particleEngine.AddInitializer(ParticleInitializers.ScatterByDirection(radius / 2f));
+		particleEngine.AddInitializer(ParticleInitializers.SetSpeed(15));
+		particleEngine.AddModifier(ParticleModifiers.ApplyMovement());
+	}
+
+	public override void OnAddedToScene()
+	{
+		Scene.AddEntity(particleEngine);
+	}
+
+	public override void OnUpdate()
+	{
+		// Movement
+		float rotationOffset = ((float)index / count) * MathF.Tau;
+		float x = MathF.Cos(Scene.Time + rotationOffset) * 64;
+		float y = MathF.Sin(Scene.Time + rotationOffset) * 64;
+		Transform.WorldRotation = Scene.Time * -100f;
+		Transform.WorldPosition = new(x, y);
+	}
+
+	private void StreamParticle(out Particle2D particleTemplate)
+	{
+		particleTemplate = new()
+		{
+			Position = Transform.WorldPosition,
+			Lifespan = 1
+		};
+	}
+}
---
+++ ClockworkExamples/ShaderExample/ShaderExample.cs
@@ -0,0 +1,26 @@
+﻿using Clockwork;
+using Clockwork.Graphics;
+using Clockwork.Graphics.Draw3D;
+using System.Numerics;
+
+public class ShaderExample : Game
+{
+	private Scene scene = new();
+
+	public ShaderExample() : base("Shader Example", 500, 500)
+	{
+		Camera3D camera = scene.AddEntity(new Camera3D(new Vector3(1, 1, 1)));
+		camera.Transform.WorldPosition = new(0, 0, -2);
+		scene.AddEntity(new DisplayMesh(Mesh.GenerateSphere(0.5f, 12, 12), Vector3.Zero));
+	}
+
+	public override void OnUpdate()
+	{
+		scene.Update();
+	}
+
+	public override void OnDraw()
+	{
+		scene.Draw();
+	}
+}
