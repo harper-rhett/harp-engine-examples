682a11b6bc411055d990500433c1bdae1f98e499
Harper Rhett <harperrhett@gmail.com>
2026-05-28 00:47:52 -0500

    starter UI example

---
+++ ClockworkExamples/UIExample/Program.cs
@@ -0,0 +1,4 @@
+﻿using Clockwork;
+
+Engine.Initialize("UI Example", 500, 500);
+Engine.Start(new UIExample());
---
+++ ClockworkExamples/UIExample/UIExample.cs
@@ -0,0 +1,22 @@
+﻿using Clockwork;
+using Clockwork.UI;
+
+public class UIExample : Game
+{
+	Scene scene;
+
+	public UIExample()
+	{
+		scene = new([new Button()]);
+	}
+
+	public override void OnUpdate()
+	{
+		scene.Update();
+	}
+
+	public override void OnDraw()
+	{
+		scene.Draw();
+	}
+}
