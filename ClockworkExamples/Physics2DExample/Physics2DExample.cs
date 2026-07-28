a86dff1e5819def41d0b2764f0fd757dc73dee42
Harper Rhett <harperrhett@gmail.com>
2026-07-28 11:43:18 -0500

    finished physics example

--- ClockworkExamples/Physics2DExample/Physics2DExample.cs
+++ ClockworkExamples/Physics2DExample/Physics2DExample.cs
@@ -3,33 +3,42 @@ using Box2D;
 using System.Numerics;
 using Clockwork.Graphics.Draw2D;
 using Clockwork.Graphics;
+using System;
+using Clockwork.Utilities;
+
+// HughPH.Box2D is the recommended library for 2D physics.
+// A physics engine my be built into Clockwork Engine in the future.
 
 public class Physics2DExample : Game
 {
 	private World world;
+	private Body groundBody;
 	private Shape groundShape;
-	private Shape fallingShape;
+	private Body boxBody;
+	private Shape boxShape;
 
 	public Physics2DExample()
 	{
-		WorldDef worldDef = new();
-		worldDef.Gravity = new Vector2(0, 980);
+		Core.LengthUnitsPerMeter = 128;
+		WorldDef worldDef = new(gravity: new Vector2(0, 100f));
 		world = new(worldDef);
 
-		groundShape = CreateBox(BodyType.Static, new Vector2(500, 650), 750, 25);
-		fallingShape = CreateBox(BodyType.Dynamic, new Vector2(500, 100), 50, 50);
+		CreateBox(out groundBody, out groundShape, BodyType.Static, new Vector2(500, 650), 750, 25, 0f);
+		CreateBox(out boxBody, out boxShape, BodyType.Dynamic, new Vector2(500, 100), 50, 50, Generate.Radians());
+		boxShape.Restitution = 0.5f;
 	}
 
-	private Shape CreateBox(BodyType type, Vector2 position, float width, float height)
+	private void CreateBox(out Body body, out Shape shape, BodyType type, Vector2 position, float width, float height, float rotation)
 	{
-		BodyDef bodyDef = new();
-		bodyDef.Type = type;
-		bodyDef.Position = position;
-		Body body = new(world, bodyDef);
+		BodyDef bodyDef = new(type: type, position: position, rotation: rotation);
+		body = new(world, bodyDef);
 
-		Polygon polygon = Polygon.MakeBox(width / 2f, height / 2f);
+		float halfWidth = width / 2f;
+		float halfHeight = height / 2f;
+		Polygon polygon = Polygon.MakeBox(halfWidth, halfHeight);
+
 		ShapeDef shapeDef = new();
-		return new(body, shapeDef, polygon);
+		shape = new Shape(body, shapeDef, polygon);
 	}
 
 	public override void OnUpdate()
@@ -40,10 +49,11 @@ public class Physics2DExample : Game
 	public override void OnDraw()
 	{
 		Drawing.Clear(Colors.Black);
-
 		DrawShape(groundShape, Colors.Blue);
-		DrawShape(fallingShape, Colors.Red);
+		DrawShape(boxShape, Colors.Red);
 	}
+
+	public override void OnDrawGUI() { }
 
 	private void DrawShape(Shape shape, Color color)
 	{
