3a62eaeaaf0cc1faa567e6b720570197ac09b158
Harper Rhett <harperrhett@gmail.com>
2026-07-29 11:10:06 -0500

    toggle button

--- ClockworkExamples/UIExample/OptionsMenuScene.cs
+++ ClockworkExamples/UIExample/OptionsMenuScene.cs
@@ -6,11 +6,15 @@ public class OptionsMenuScene : Scene
 {
 	private UIExample uiExample;
 	private VerticalStackContainer rootContainer;
+	private TextButton backButton;
+	private TextToggle fullscreenToggle;
 
-	public OptionsMenuScene(UIExample uiExample)
+	public OptionsMenuScene(UIExample uiExample) : base(Colors.Gray)
 	{
 		this.uiExample = uiExample;
 		CreateContainer();
+		CreateFullscreenToggle();
+		CreateBackButton();
 	}
 
 	private void CreateContainer()
@@ -24,5 +28,26 @@ public class OptionsMenuScene : Scene
 
 		Panel panel = AddEntity(new Panel(rootContainer, DrawContext.Game));
 		panel.HorizontalAlignment = HorizontalAlignment.Center;
+	}
+
+	// Need to add slider
+
+	private void CreateFullscreenToggle()
+	{
+		fullscreenToggle = new TextToggle("FULLSCREEN");
+		fullscreenToggle.Toggled += uiExample.SetFullscreen;
+		rootContainer.AddChild(fullscreenToggle);
+	}
+
+	private void CreateBackButton()
+	{
+		backButton = new("BACK");
+		backButton.Released += uiExample.CloseOptions;
+		rootContainer.AddChild(backButton);
+	}
+
+	public void Dispose()
+	{
+		backButton.Released -= uiExample.CloseOptions;
 	}
 }
--- ClockworkExamples/UIExample/UIExample.cs
+++ ClockworkExamples/UIExample/UIExample.cs
@@ -1,19 +1,19 @@
 ﻿using Clockwork;
 using Clockwork.Graphics.Text;
 using Clockwork.Windowing;
+using System;
 
 public class UIExample : Game
 {
 	private Scene activeScene;
-	private Scene mainMenu;
-	private Scene optionsMenu;
+	private MainMenuScene mainMenu;
+	private OptionsMenuScene optionsMenu;
 
 	public UIExample()
 	{
 		Window.SetResizable(true);
 		Font.Default = Font.Load("Rajdhani-Bold.ttf", 100);
 		mainMenu = new MainMenuScene(this);
-		optionsMenu = new OptionsMenuScene(this);
 		activeScene = mainMenu;
 	}
 
@@ -35,12 +35,24 @@ public class UIExample : Game
 	public void Play()
 	{
 		activeScene = new GameScene();
+		mainMenu.Dispose();
 		mainMenu = null;
+		if (optionsMenu is not null) optionsMenu.Dispose();
 		optionsMenu = null;
 	}
 
 	public void OpenOptions()
 	{
+		if (optionsMenu is null) optionsMenu = new OptionsMenuScene(this);
 		activeScene = optionsMenu;
 	}
+
+	public void SetFullscreen(bool isFullscreen) => Window.SetFullscreen(isFullscreen);
+
+	public void CloseOptions()
+	{
+		activeScene = mainMenu;
+	}
+
+	public void Exit() => Environment.Exit(0);
 }
